ad46ed16d57c5537d8710bf8bf7223fca679cb44
Grigoryan <argishtigrigoryan1991@gmail.com>
2021-04-21 02:08:54 +0400

    first work

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+//        int x = 6;
+//        if (x % 2 == 0) {
+//            System.out.print(true);
+//        } else {
+//            System.out.print(false);
+//        }
+        int sum=0;
+        for (int i = 1; i <= 20; ++i) {
+               sum=sum+i;
+        }
+        System.out.print(sum);
+    }
+}
+
