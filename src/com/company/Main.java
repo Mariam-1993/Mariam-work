2f54948dbfad3d5799ef8944c0fced8a5b6ca710
Grigoryan <argishtigrigoryan1991@gmail.com>
2021-05-22 12:43:48 +0400

    example

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,20 +1,16 @@
 package com.company;
 
 public class Main {
-
     public static void main(String[] args) {
-        int x = 6;
-        if (x % 2 == 0) {
-            System.out.print(true);
-        } else {
-            System.out.print(false);
+       int[] myArr={80,50,50,40,60,80,100};
+       for(int i=0;i<myArr.length; i++){
+        for(int j=i+1;j<myArr.length; j++) {
+            if(myArr[i]==myArr[j]) {
+                System.out.print(myArr[i]);
+            }
         }
+       }
 
-        int sum = 0;
-        for (int i = 1; i <= 20; ++i) {
-            sum = sum + i;
-        }
-        System.out.print(sum);
+
     }
 }
-
